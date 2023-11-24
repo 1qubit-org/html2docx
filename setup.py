b8f3ba5e4d8262668cc03e8059106884741ea3f2
Alexander Bluhm <lamiy-develop@live.de>
2023-11-24 21:55:43 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='html2docx',
-    version='1.0.0',
+    version='1.0.1',
     url='https://github.com/1qubit-org/html2docx',
     packages=find_packages(),
     install_requires=[
